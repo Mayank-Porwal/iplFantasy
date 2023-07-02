5b45af49c1dea7e7fb20a0a0b2db6f2de400586e
Mayank-Porwal <mayankporwal011@gmail.com>
2023-07-02 10:00:29 +0530

    Fixed leagues routes and added swagger

--- app/leagues/routes.py
+++ app/leagues/routes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from flask import Blueprint, request
 from sqlalchemy import func
-from app.models import UserLeague, UserTeam, LeagueInfo
+from app.models import UserLeague, UserTeam, LeagueInfo, User
 from app import db
 from app.leagues.utils import LeagueType, generate_uuid
 
@@ -12,18 +12,20 @@ leagues = Blueprint('leagues', __name__)
 def create_league():
     payload = request.get_json()
     name = payload.get('name')
-    owner = payload.get('owner')
+    owner_name = payload.get('owner_name')
     league_type = payload.get('type')
-    print(league_type)
 
-    if UserLeague.query.filter_by(name=name, owner=owner).first():
+    owner = User.query.filter_by(username=owner_name).first()
+    if not owner:
+        return {'message': f'{owner_name} does not exist'}, 403
+
+    if UserLeague.query.filter_by(name=name, owner=int(owner.id)).first():
         return {'message': f'{name} already exists'}, 409
 
-    if league_type == LeagueType.private:
-        join_code = generate_uuid()
-        league = UserLeague(name=name, owner=owner, league_type=league_type, join_code=join_code)
+    if league_type == LeagueType.private.name:
+        league = UserLeague(name=name, owner=int(owner.id), league_type=league_type, join_code=generate_uuid())
     else:
-        league = UserLeague(name=name, owner=owner, league_type=league_type)
+        league = UserLeague(name=name, owner=int(owner.id), league_type=league_type)
 
     db.session.add(league)
     db.session.commit()
@@ -33,38 +35,39 @@ def create_league():
 @leagues.route('/join_league', methods=['POST'])
 def join_league():
     payload = request.get_json()
-    team_id = payload.get('team_id')
+    team_name = payload.get('team_name')
     join_code = payload.get('code')
-    user_id = payload.get('user_id')
+    user_name = payload.get('user_name')
     league_type = payload.get('type')
-    league_id = payload.get('league_id')
+    league_name = payload.get('league_name')
 
     # Checking if the league exists or not
-    league = UserLeague.query.filter_by(id=league_id).first()
-    print(league)
+    league = UserLeague.query.filter_by(name=league_name).first()
     if not league:
         return {'message': 'League does not exist. Please create one to join.'}, 422
 
     # If the league is private, validate the join code
-    if league_type == LeagueType.private:
+    if league_type == LeagueType.private.name:
         if league.join_code != join_code:
             return {'message': 'The join code is incorrect. Please try again.'}, 422
 
     # Checking if user has already joined the league with current team
-    league_info = LeagueInfo.query.filter_by(league_id=league.id, user_id=user_id).first()
-    print(league_info)
-
-    if league_info:
-        if league_info.team_id == team_id:
-            return {'message': f"You've already joined {league.name} with this team."}, 409
-        else:
-            return {'message': f"You can't join with multiple teams in the same league."}, 409
+    user = User.query.filter_by(username=user_name).first()
+    if not user:
+        return {'message': f'{user_name} does not exist'}, 403
+    league_info = LeagueInfo.query.filter_by(league_id=league.id, user_id=user.id).first()
 
     # Check if the team trying to join exists or not and the user is the owner or not
-    team = UserTeam.query.filter_by(id=int(team_id), user_id=user_id).first()
+    team = UserTeam.query.filter_by(name=team_name, user_id=user.id).first()
 
     if team:
-        join = LeagueInfo(league_id=league.id, user_id=payload.get('user_id'), team_id=team_id)
+        if league_info:
+            if league_info.team_id == team.id:
+                return {'message': f"You've already joined {league.name} with {team_name}."}, 409
+            else:
+                return {'message': f"You can't join with multiple teams in the same league."}, 409
+
+        join = LeagueInfo(league_id=league.id, user_id=user.id, team_id=team.id)
         db.session.add(join)
         db.session.commit()
         return {'message': f'Successfully joined the league: {league.name} with team: {team.name}.'}, 201
@@ -75,16 +78,19 @@ def join_league():
 @leagues.route('/delete_league', methods=['POST'])
 def delete_league():
     payload = request.get_json()
-    league_id = payload.get('league_id')
-    owner = payload.get('owner')
+    league_name = payload.get('league_name')
+    owner_name = payload.get('owner_name')
 
-    league = UserLeague.query.filter_by(id=league_id).first()
-    league_name = league.name
+    league = UserLeague.query.filter_by(name=league_name).first()
+    owner = User.query.filter_by(username=owner_name).first()
 
     if league:
-        if league.owner == int(owner):
-            UserLeague.query.filter_by(id=league_id).delete()
-            LeagueInfo.query.filter_by(league_id=league_id).delete()
+        league_name = league.name
+        if not owner:
+            return {'message': f'User {owner_name} does not exist.'}, 403
+        if league.owner == int(owner.id):
+            LeagueInfo.query.filter_by(league_id=league.id).delete()
+            UserLeague.query.filter_by(id=league.id).delete()
             db.session.commit()
             return {'message': f'{league_name} deleted successfully.'}, 201
         return {'message': f'League can be deleted by its owner only.'}, 403
