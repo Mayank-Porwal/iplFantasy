66adc47a1af6628394b924c5ff01b53edf03a4c5
Mayank-Porwal <mayankporwal011@gmail.com>
2024-03-22 09:18:34 +0530

    sched changes

---
+++ app/scheduled_jobs.py
@@ -0,0 +1,27 @@
+from app.dao.leagues import LeagueDAO
+from app.dao.match import MatchDAO
+from app.models.leagues import League
+from app.models.match import Match
+from app.service.snapshot import SnapshotService
+from app import scheduler
+from app import create_app
+
+snapshot_service = SnapshotService()
+
+
+@scheduler.task(id='abc', trigger='date', run_date='2024-03-22 09:09:59')
+def submit_all_teams_for_all_leagues():
+    with create_app().app_context():
+        match: Match = MatchDAO.get_current_match_id_by_status()
+        leagues: list[League] = LeagueDAO.get_all_active_leagues()
+        print(match.id)
+        print('started')
+
+        try:
+            # snapshot_service.submit_all_teams_for_league(match.id, 53)
+            for league in leagues:
+                print(f'League id: {league.id}')
+                snapshot_service.submit_all_teams_for_league(match.id, league.id)
+            return {'message': 'Cron job to submit all teams for all leagues ran successfully'}
+        except Exception as e:
+            return {'message': f'Failed with error: {e}'}
--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 from app import create_app
+from app import scheduler
+from flask import current_app
+from app.scheduled_jobs import submit_all_teams_for_all_leagues
 
 if __name__ == '__main__':
-    create_app().run(debug=True, port=8000)
+    # with current_app.app_context():
+    #     scheduler.add_job(
+    #         id='submit',
+    #         func=submit_all_teams_for_all_leagues,
+    #         trigger='date',
+    #         run_date='2024-03-21 18:57:55'
+    #     )
+    #     scheduler.start()
+    context = create_app()
+    scheduler.init_app(context)
+    scheduler.start()
+    context.run(debug=True, port=8000, use_reloader=False)
